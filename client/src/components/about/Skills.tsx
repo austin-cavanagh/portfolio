265450c445f857d11febe90a3312c8f5156b4dc7
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-10 18:15:49 -0800

    Added all icons to Skills componnent and organized based on category

--- client/src/components/about/Skills.tsx
+++ client/src/components/about/Skills.tsx
@@ -8,17 +8,29 @@ const frontend: TechIconName[] = [
   'Tailwind',
   'React',
   'Redux',
-  'Vue',
-  'Solid',
-  'Svelte',
+  //   'Threejs',
+  //   'Vue',
+  //   'Solid',
+  //   'Svelte',
 ];
 
-const backend: TechIconName[] = ['Node.js', 'Express', 'Python', 'Pandas'];
+const backend: TechIconName[] = ['Node.js', 'Express', 'Python'];
+
+const databases: TechIconName[] = [
+  'PostgreSQL',
+  'MySQL',
+  'MongoDB',
+  'Mongoose',
+];
+
+const devOps: TechIconName[] = ['Docker', 'Kubernetes', 'AWS', 'Google Cloud'];
+
+const misc: TechIconName[] = ['Next.js', 'Jest', 'Cypress', 'Webpack'];
 
 function Skills() {
   return (
-    <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
-      <div>
+    <div className="space-y-6">
+      <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
         <h2 className="text-lg text-white">Frontend</h2>
         <div className="flex">
           {frontend.map(name => {
@@ -26,12 +38,44 @@ function Skills() {
           })}
         </div>
       </div>
-      <div>
-        <h2 className="text-lg text-white">Backend</h2>
-        <div className="flex">
-          {backend.map(name => {
-            return <TechIcon name={name} key={name} />;
-          })}
+
+      <div className="flex">
+        <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
+          <h2 className="text-lg text-white">Backend</h2>
+          <div className="flex">
+            {backend.map(name => {
+              return <TechIcon name={name} key={name} />;
+            })}
+          </div>
+        </div>
+
+        <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
+          <h2 className="text-lg text-white">Databases</h2>
+          <div className="flex">
+            {databases.map(name => {
+              return <TechIcon name={name} key={name} />;
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="flex">
+        <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
+          <h2 className="text-lg text-white">DevOps</h2>
+          <div className="flex">
+            {devOps.map(name => {
+              return <TechIcon name={name} key={name} />;
+            })}
+          </div>
+        </div>
+
+        <div className="flex flex-col rounded-3xl bg-gray-800 p-5">
+          <h2 className="text-lg text-white">Misc</h2>
+          <div className="flex">
+            {misc.map(name => {
+              return <TechIcon name={name} key={name} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
