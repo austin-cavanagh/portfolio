1159e909fd620d63a2718ff56e1cb62140d77972
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-10 17:23:23 -0800

    Created techIcons object to hold all of the svgs, will be called on each icon render

--- client/src/components/about/Skills.tsx
+++ client/src/components/about/Skills.tsx
@@ -1,13 +1,19 @@
-import CSS3Icon from '../../assets/tech-icons/CSS3Icon';
-import HTML5Icon from '../../assets/tech-icons/HTML5Icon';
-import JavaScriptIcon from '../../assets/tech-icons/JavaScriptIcon';
-import ReactIcon from '../../assets/tech-icons/ReactIcon';
-import ReduxIcon from '../../assets/tech-icons/ReduxIcon';
-import SolidIcon from '../../assets/tech-icons/SolidIcon';
-import SvelteIcon from '../../assets/tech-icons/SvelteIcon';
-import TailwindIcon from '../../assets/tech-icons/TailwindIcon';
-import TypeScriptIcon from '../../assets/tech-icons/TypeScriptIcon';
-import VueIcon from '../../assets/tech-icons/VueIcon';
+import TechIcon, { TechIconName } from './TechIcon';
+
+const frontend: TechIconName[] = [
+  'JavaScript',
+  'TypeScript',
+  'HTML5',
+  'CSS3',
+  'Tailwind',
+  'React',
+  'Redux',
+  'Vue',
+  'Solid',
+  'Svelte',
+];
+
+const backend: TechIconName[] = ['Node.js', 'Express', 'Python', 'Pandas'];
 
 function Skills() {
   return (
@@ -15,29 +21,18 @@ function Skills() {
       <div>
         <h2 className="text-lg text-white">Frontend</h2>
         <div className="flex">
-          <JavaScriptIcon className="" />
-          <TypeScriptIcon className="" />
-          <HTML5Icon className="" />
-          <CSS3Icon className="" />
-          <TailwindIcon className="" />
-          <ReactIcon className="" />
-          <ReduxIcon className="" />
-          <VueIcon className="" />
-          <SolidIcon className="" />
-          <SvelteIcon className="" />
+          {frontend.map(name => {
+            return <TechIcon name={name} key={name} />;
+          })}
         </div>
       </div>
       <div>
         <h2 className="text-lg text-white">Backend</h2>
-        <div className="flex"></div>
-      </div>
-      <div>
-        <h2 className="text-lg text-white">Databases</h2>
-        <div className="flex"></div>
-      </div>
-      <div>
-        <h2 className="text-lg text-white">Other</h2>
-        <div className="flex"></div>
+        <div className="flex">
+          {backend.map(name => {
+            return <TechIcon name={name} key={name} />;
+          })}
+        </div>
       </div>
     </div>
   );
