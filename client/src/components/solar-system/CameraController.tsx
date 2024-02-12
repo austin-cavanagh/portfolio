3ac28336e2280a2feb6cda650adfe953b09e1437
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-02-11 22:27:56 -0800

    Tween is now moving the camera to planets from overview position

--- client/src/components/solar-system/CameraController.tsx
+++ client/src/components/solar-system/CameraController.tsx
@@ -44,15 +44,18 @@ function CameraController({}: CameraControllerProps) {
   useEffect(() => {
     if (!currentPlanetRef?.current) return;
 
+    const planetRadius = currentPlanetRef.current.geometry.parameters.radius;
     const planetPosition = currentPlanetRef.current.position;
-    const newPosition = planetPosition.clone().add(new Vector3(100, 0, 0));
+    const newPosition = planetPosition
+      .clone()
+      .add(new Vector3(planetRadius * 4, 0, 0));
     const newTarget = planetPosition;
 
     new TWEEN.Tween(camera.position)
       .to(newPosition, 2000)
       .easing(TWEEN.Easing.Quadratic.InOut)
       .onUpdate(() => {
-        // camera.updateProjectionMatrix();
+        camera.updateProjectionMatrix();
       })
       .start();
 
@@ -60,11 +63,9 @@ function CameraController({}: CameraControllerProps) {
       .to(newTarget, 2000)
       .easing(TWEEN.Easing.Quadratic.InOut)
       .onUpdate(() => {
-        // orbitControlsRef.current.update();
+        orbitControlsRef.current.update();
       })
       .start();
-
-    console.log('running');
   }, [currentPlanet]);
 
   useEffect(() => {
@@ -74,98 +75,98 @@ function CameraController({}: CameraControllerProps) {
   useFrame(({}, delta) => {
     TWEEN.update();
 
-    if (currentPlanet === 'Overview') {
-      // During Overview transition
-      if (transitionProgressRef.current < 0.12) {
-        const newCameraPosition = new Vector3(-750, 1000, 1500);
-        const newTargetPosition = new Vector3(0, 0, 0);
+    // if (currentPlanet === 'Overview') {
+    //   // During Overview transition
+    //   if (transitionProgressRef.current < 0.12) {
+    //     const newCameraPosition = new Vector3(-750, 1000, 1500);
+    //     const newTargetPosition = new Vector3(0, 0, 0);
 
-        transitionProgressRef.current += delta * 0.04;
+    //     transitionProgressRef.current += delta * 0.04;
 
-        camera.position.lerp(newCameraPosition, transitionProgressRef.current);
+    //     camera.position.lerp(newCameraPosition, transitionProgressRef.current);
 
-        orbitControlsRef.current.target.lerp(
-          newTargetPosition,
-          transitionProgressRef.current,
-        );
-      }
-
-      // After Overview transition
-      if (transitionProgressRef.current >= 0.12 && isTransitioning) {
-        // console.log('done');
-
-        dispatch(endTransition());
-        // console.log('ended transition');
-      }
-
-      return;
-    }
-
-    const planetGeometry = currentPlanetRef.current.geometry as SphereGeometry;
-    const planetRadius = planetGeometry.parameters.radius;
-
-    // During planet transition
-    // Solution goes in this if statement
-    // if (currentPlanet && transitionProgressRef.current < 1) {
-    //   const start = camera.position.clone();
-
-    //   const planetPosition = currentPlanetRef.current.position;
-
-    //   currentPlanetRef.current.getWorldPosition(planetPosition);
-
-    //   const offsetEnd = planetPosition
-    //     .clone()
-    //     .add(new Vector3(0, 0, planetRadius * 6));
-
-    //   const dynamicHeightAdjustment =
-    //     transitionProgressRef.current < 0.5
-    //       ? (0.5 - transitionProgressRef.current) * 2
-    //       : transitionProgressRef.current - 0.5;
-
-    //   const controlPointHeight =
-    //     planetRadius + dynamicHeightAdjustment * planetRadius * 2;
-
-    //   const controlPoint = new Vector3(
-    //     (start.x + offsetEnd.x) / 2,
-    //     Math.max(start.y, offsetEnd.y) + controlPointHeight,
-    //     (start.z + offsetEnd.z) / 2,
-    //   );
-
-    //   const t = transitionProgressRef.current;
-    //   const bezierPoint = calculateBezierPoint(
-    //     t,
-    //     start,
-    //     controlPoint,
-    //     offsetEnd,
-    //   );
-
-    //   camera.position.lerp(bezierPoint, 0.1);
-    //   orbitControlsRef.current.target.lerp(planetPosition, t);
-
-    //   transitionProgressRef.current += delta * 0.25;
-    // }
-
-    // After planet transition
-    // if (currentPlanet && transitionProgressRef.current > 1) {
-    //   if (isTransitioning) dispatch(endTransition());
-
-    //   const planetPosition = new Vector3();
-    //   currentPlanetRef.current.getWorldPosition(planetPosition);
-    //   orbitControlsRef.current.target.lerp(planetPosition, 0.1);
-    //   const direction = new Vector3()
-    //     .subVectors(camera.position, planetPosition)
-    //     .normalize();
-
-    //   const distance = planetRadius * 6;
-    //   const desiredCameraPosition = new Vector3().addVectors(
-    //     planetPosition,
-    //     direction.multiplyScalar(distance),
-    //   );
-
-    //   if (showContent) {
-    //     camera.position.lerp(desiredCameraPosition, 0.05);
+    //     orbitControlsRef.current.target.lerp(
+    //       newTargetPosition,
+    //       transitionProgressRef.current,
+    //     );
     //   }
+
+    //   // After Overview transition
+    //   if (transitionProgressRef.current >= 0.12 && isTransitioning) {
+    //     // console.log('done');
+
+    //     dispatch(endTransition());
+    //     // console.log('ended transition');
+    //   }
+
+    //   return;
     // }
+
+    // const planetGeometry = currentPlanetRef.current.geometry as SphereGeometry;
+    // const planetRadius = planetGeometry.parameters.radius;
+
+    // // During planet transition
+    // // Solution goes in this if statement
+    // // if (currentPlanet && transitionProgressRef.current < 1) {
+    // //   const start = camera.position.clone();
+
+    // //   const planetPosition = currentPlanetRef.current.position;
+
+    // //   currentPlanetRef.current.getWorldPosition(planetPosition);
+
+    // //   const offsetEnd = planetPosition
+    // //     .clone()
+    // //     .add(new Vector3(0, 0, planetRadius * 6));
+
+    // //   const dynamicHeightAdjustment =
+    // //     transitionProgressRef.current < 0.5
+    // //       ? (0.5 - transitionProgressRef.current) * 2
+    // //       : transitionProgressRef.current - 0.5;
+
+    // //   const controlPointHeight =
+    // //     planetRadius + dynamicHeightAdjustment * planetRadius * 2;
+
+    // //   const controlPoint = new Vector3(
+    // //     (start.x + offsetEnd.x) / 2,
+    // //     Math.max(start.y, offsetEnd.y) + controlPointHeight,
+    // //     (start.z + offsetEnd.z) / 2,
+    // //   );
+
+    // //   const t = transitionProgressRef.current;
+    // //   const bezierPoint = calculateBezierPoint(
+    // //     t,
+    // //     start,
+    // //     controlPoint,
+    // //     offsetEnd,
+    // //   );
+
+    // //   camera.position.lerp(bezierPoint, 0.1);
+    // //   orbitControlsRef.current.target.lerp(planetPosition, t);
+
+    // //   transitionProgressRef.current += delta * 0.25;
+    // // }
+
+    // // After planet transition
+    // // if (currentPlanet && transitionProgressRef.current > 1) {
+    // //   if (isTransitioning) dispatch(endTransition());
+
+    // //   const planetPosition = new Vector3();
+    // //   currentPlanetRef.current.getWorldPosition(planetPosition);
+    // //   orbitControlsRef.current.target.lerp(planetPosition, 0.1);
+    // //   const direction = new Vector3()
+    // //     .subVectors(camera.position, planetPosition)
+    // //     .normalize();
+
+    // //   const distance = planetRadius * 6;
+    // //   const desiredCameraPosition = new Vector3().addVectors(
+    // //     planetPosition,
+    // //     direction.multiplyScalar(distance),
+    // //   );
+
+    // //   if (showContent) {
+    // //     camera.position.lerp(desiredCameraPosition, 0.05);
+    // //   }
+    // // }
   });
 
   return (
