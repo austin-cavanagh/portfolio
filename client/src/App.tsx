d9ad1711c6d96843a900bc204f5ba67f64b76659
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-03 00:19:39 -0700

    Saving version of code before deleting the Introdocustion.tsx and Intro.tsx files that contained the tying and light speed animations - decided to not use them and chose a simpler path for the user to get to the core content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 // import SolarSystem from './components/solar-system/SolarSystem';
-// import Introduction from './containers/Introduction';
-// import Intro from './containers/Intro';
+import Introduction from './containers/Introduction';
+import Intro from './containers/Intro';
 // import { useSelector } from 'react-redux';
 // import { RootState } from './state/store';
 import LoadingScreen from './components/solar-system/LoadingScreen';
@@ -8,13 +8,17 @@ import LoadingScreen from './components/solar-system/LoadingScreen';
 function App() {
   // const { scene } = useSelector((state: RootState) => state.app);
 
+  const scene = 'typing';
+
   return (
     <>
-      {/* {scene === 'light-speed' && <Introduction />}
-      {scene === 'typing' && <Intro />}
-      {scene === 'solar-system' && <SolarSystem />} */}
+      {/* {scene === 'light-speed' && <Introduction />} */}
+      <div className="h-full w-full bg-black">
+        {scene === 'typing' && <Intro />}
+      </div>
+      {/* {scene === 'solar-system' && <SolarSystem />} */}
 
-      <LoadingScreen />
+      {/* <LoadingScreen /> */}
     </>
   );
 }
