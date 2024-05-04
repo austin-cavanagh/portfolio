d27f7fd9f588442f1e6ef9c84c61d9ce81c6473d
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-04 15:48:22 -0700

    Added animations from framer motion to show the content moving up when we reach the planet after a transition

--- client/src/containers/UserInterface.tsx
+++ client/src/containers/UserInterface.tsx
@@ -3,11 +3,13 @@ import { RootState } from '../state/store';
 
 import Navbar from '../components/user-interface/Navbar';
 import BottomBar from '../components/user-interface/Footer';
-// import About from './About';
+import About from './About';
 // import Contact from './Contact';
 // import ProjectOne from '../components/projects/ProjectOne';
 // import ProjectTwo from '../components/projects/ProjectTwo';
 // import ProjectThree from '../components/projects/ProjectsThree';
+
+import { motion } from 'framer-motion';
 
 type UserInterfaceProps = {};
 
@@ -15,13 +17,30 @@ function UserInterface({}: UserInterfaceProps) {
   const { currentPlanet } = useSelector((state: RootState) => state.app);
 
   const renderContent = () => {
+    const { isTransitioning } = useSelector((state: RootState) => state.app); // Assuming this flag exists and is managed appropriately
+
+    const contentVariants = {
+      hidden: { opacity: 0, y: 50 }, // starts 50px below the final position
+      visible: { opacity: 1, y: 0 },
+    };
+
     switch (currentPlanet) {
-      // case 'Earth':
-      //   return (
-      //     <div className="pointer-events-auto flex flex-1 items-center justify-center overflow-auto">
-      //       <About />;
-      //     </div>
-      //   );
+      case 'Earth':
+        return (
+          // <div className="pointer-events-auto flex flex-1 items-center justify-center overflow-auto">
+          //   <About />;
+          // </div>
+
+          <motion.div
+            className="pointer-events-auto flex flex-1 items-center justify-center overflow-auto"
+            initial="hidden"
+            animate={!isTransitioning ? 'visible' : 'hidden'}
+            variants={contentVariants}
+            transition={{ duration: 0.5, ease: 'easeOut' }}
+          >
+            <About />
+          </motion.div>
+        );
       // case 'Mars':
       //   return (
       //     <div className="pointer-events-auto flex flex-1 items-center justify-center overflow-auto">
