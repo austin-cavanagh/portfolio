131140bbf32b380019e3e9ee3527cf953df56e2e
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-05 16:57:00 -0700

    Fixed build errors to enable the project to be build for production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,29 @@
 import SolarSystem from './components/solar-system/SolarSystem';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from './state/store';
-import LoadingScreen from './components/solar-system/LoadingScreen';
-import { Suspense, useEffect, useState } from 'react';
-import { setIsLoading } from './state/appSlice';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { RootState } from './state/store';
+// import LoadingScreen from './components/solar-system/LoadingScreen';
+// import { useEffect, useState } from 'react';
+// import { setIsLoading } from './state/appSlice';
 
 function App() {
   // const { scene } = useSelector((state: RootState) => state.app);
 
-  const { isLoading } = useSelector((state: RootState) => state.app);
-  const dispatch = useDispatch();
+  // const {} = useSelector((state: RootState) => state.app);
+  // const dispatch = useDispatch();
 
-  const [readyToShow, setReadyToShow] = useState(false);
+  // const [readyToShow, setReadyToShow] = useState(false);
 
-  useEffect(() => {
-    dispatch(setIsLoading(true)); // Start the loading state
-    // Here you would typically initialize SolarSystem or load resources
-    setTimeout(() => {
-      setReadyToShow(true); // Signal that resources are ready
-      setTimeout(() => {
-        // setLoading(false); // After a slight delay, hide the loading screen
-        dispatch(setIsLoading(false));
-      }, 500); // Give some time for the user to recognize the loaded state
-    }, 3000); // Simulate a loading process
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(setIsLoading(true)); // Start the loading state
+  //   // Here you would typically initialize SolarSystem or load resources
+  //   setTimeout(() => {
+  //     setReadyToShow(true); // Signal that resources are ready
+  //     setTimeout(() => {
+  //       // setLoading(false); // After a slight delay, hide the loading screen
+  //       dispatch(setIsLoading(false));
+  //     }, 500); // Give some time for the user to recognize the loaded state
+  //   }, 3000); // Simulate a loading process
+  // }, [dispatch]);
 
   return (
     <>
