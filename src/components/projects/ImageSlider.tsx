5500a84b1f9cd45582aaa8765d1751d596372768
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2024-05-10 00:39:29 -0700

    Created a file labeled projectScreenshots.ts to hold the screenshots for each project in a single locaiton

--- src/components/projects/ImageSlider.tsx
+++ src/components/projects/ImageSlider.tsx
@@ -1,29 +1,14 @@
 import { Carousel } from 'flowbite-react';
 
+import { b2cEcommerceWebsiteScreenshots } from '../../assets/projectScreenshots';
+
 export default function Component() {
   return (
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel pauseOnHover>
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-          alt="..."
-        />
-        <img
-          src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-          alt="..."
-        />
+        {b2cEcommerceWebsiteScreenshots.map((image, index) => {
+          return <img key={index} src={image.src} alt={image.alt} />;
+        })}
       </Carousel>
     </div>
   );
